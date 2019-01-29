5249fd72084e39ae4582807f3a021c5e74ad3066
Mahima-M-A <43963605+Mahima-M-A@users.noreply.github.com>
2019-01-29 16:41:52 +0530

    Update binarySearchTree.c

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -11,21 +11,7 @@ struct node
      struct node* right;
 };
 
-
-// function to search a given key in a given BST
-struct node* search(struct node* root, int key)
-{
-    // Base Cases: root is null or key is present at root
-    if (root == NULL || root->key == key)
-       return root;
-
-    // Key is greater than root's key
-    if (root->key < key)
-       return search(root->right, key);
-
-    // Key is smaller than root's key
-    return search(root->left, key);
-}
+//Function to insert the tree elements in a sorted manner
 
 struct node* insert(struct node*r, int x)
 {
@@ -40,43 +26,6 @@ struct node* insert(struct node*r, int x)
             r->left = insert(r->left, x);
 	else if (x > r->key)
             r->right = insert(r->right, x);
-	return r;
-}
-
-struct node* findmin(struct node*r)
-{
-	if (r == NULL)
-		return NULL;
-	else if (r->left!=NULL)
-		return findmin(r->left);
-	else if (r->left == NULL)
-		return r;
-}
-
-struct node* del(struct node*r, int x)
-{
-	struct node *t;
-	if(r == NULL)
-		printf("\nElement not found");
-	else if (x < r->key)
-            r->left = del(r->left, x);
-	else if (x > r->key)
-            r->right = del(r->right, x);
-	else if ((r->left != NULL) && (r->right != NULL))
-	{
-            t = findmin(r->right);
-            r->key = t->key;
-            r->right = del(r->right, r->key);
-	}
-	else
-	{
-            t = r;
-            if (r->left == NULL)
-                r = r->right;
-            else if (r->right == NULL)
-                r = r->left;
-            free(t);
-	}
 	return r;
 }
 
@@ -150,19 +99,16 @@ void printPreorder(struct node* node)
 int main()
 {
     struct node* root = NULL;
-    struct node* LOC=NULL;
     int x, c = 1, z;
     int element;
   
     while(1)
     {
         printf("\n1 Insert an element ");
-        printf("\n2 Delete an element");
-        printf("\n3 Search for an element ");
-        printf("\n4 Display");
-        printf("\n5 To print the preorder");
-        printf("\n6 To print the inorder");
-        printf("\n7 To print the postorder");
+        printf("\n2 Display");
+        printf("\n3 To print the preorder");
+        printf("\n4 To print the inorder");
+        printf("\n5 To print the postorder");
         printf("\n-1 Exit ");
         printf("\nEnter your choice: ");
         scanf("%d", &c);
@@ -173,21 +119,7 @@ int main()
                 scanf("%d", &z);
                 root = insert(root,z);
                 break;
-            case 2:
-                printf("\nEnter the info to be deleted:");
-                scanf("%d", &z);
-                root = del(root, z);
-                break;
-            case 3:
-                printf("\nEnter element to be searched:  ");
-                scanf("%d", &element);
-                LOC=search(root, element);
-                if(LOC != NULL)
-                    printf("\n%d found in Binary Search Tree !!\n",element);
-                else
-                    printf("\nIt is not present in Binary Search Tree\n");
-                break;
-            case 4 : 
+            case 2: 
                 if(root == NULL)
                     printf("\nTree empty");
                 else
@@ -197,15 +129,15 @@ int main()
                     display(root,1);
                 }
                 break;
-            case 5:
+            case 3:
                 printf("\nPreorder traversal of binary tree is \n");
                 printPreorder(root);
                 break;
-            case 6:
+            case 4:
                 printf("\nInorder traversal of binary tree is \n");
                 printInorder(root);
                 break;
-            case 7:
+            case 5:
                 printf("\nPostorder traversal of binary tree is \n");
                 printPostorder(root);
                 break;
